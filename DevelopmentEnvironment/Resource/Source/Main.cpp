8f6dd6daf839d260b26c29eb843d57125847cece
kuroneko96 <kaityou5555@gmail.com>
2020-12-08 20:46:54 +0900

    [setup]Project開始にあたって必要なものを作成

---
+++ DevelopmentEnvironment/Resource/Source/Main.cpp
@@ -0,0 +1,14 @@
+#include <DxLib.h>
+
+/******************************************************************************
+制作者 　　　  : 佐々木海星(かいちょー)
+Project開始日  : 2020/12/8
+バージョン履歴 :
+
+******************************************************************************/
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow) {
+
+	return 0;
+}
